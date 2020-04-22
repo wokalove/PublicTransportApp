ba4a87f5763fe85bb81597d54dceccdd21de63d4
wokalove <64095660+wokalove@users.noreply.github.com>
2020-04-22 22:05:14 +0200

    Update projektMPK.py

--- projektMPK.py
+++ projektMPK.py
@@ -1,14 +1,39 @@
 import sqlite3 
+import numpy as np
 
+
+# define Python user-defined exceptions
+class Error(Exception):
+   """Base class for other exceptions"""
+   pass
+class Traveler:
+    adult=3.40
+    student=1.70
+    journeyCosts=[]
 def checkInput(lineNumbers):
-    chooseLine= input("Choose line from lines presented ABOVE:")
-    print(chooseLine)
-    if chooseLine not in lineNumbers:
-        print("Wrong input!")
-        return checkInput(lineNumbers)
-    else:
-        return chooseLine
+        chooseLine= input("Choose line from lines presented ABOVE:")
     
+        if chooseLine not in lineNumbers:
+            print("Wrong input!")
+            return checkInput(lineNumbers)
+        else:
+            return chooseLine
+            
+def ticketCosts():
+    choice=input("Are you a student? Type 'yes' or 'no'.\n").lower()   
+    try:
+        if(choice == "yes"):
+            cost = Traveler.journeyCosts.append(Traveler.student)
+        elif(choice=="no"):
+           cost= Traveler.journeyCosts.append(Traveler.adult)
+        else:
+            raise Error
+    except Error:
+        print("Wrong input, try again!")
+        checkIfStudent()
+    if choice in ["yes","no"]:
+        finalCosts=np.sum(Traveler.journeyCosts)
+        print("Your all travel costs:",finalCosts)
     
 def busStopsDisplay(busStops,inpFrom,inpTo):
     fromIndex=busStops.index(inpFrom)
@@ -32,22 +57,24 @@ def connectToData():
     connection = sqlite3.connect("rozklady.sqlite3") 
     
     crsr = connection.cursor() 
+    
     inpFrom = input("Give station from:")
     inpTo = input("Give station to:")
+    
     
     #wykonuje zapytanie w sql
     numberLine=crsr.execute("SELECT DISTINCT LineName FROM StopDepartures WHERE StopName=? AND LineName IN (SELECT LineName from StopDepartures  where StopName=?) COLLATE NOCASE",(inpFrom,inpTo,))
     #zwraca wszystkie rekordy znalezione na wskutek zapytania
     returnNumberLine= crsr.fetchall()  
     
+    #tablica do przechowywania numerów linii, które doprowadzą do celu
     lineNumber=[]
-    print("You can reach your destination by lines:")
     for i in returnNumberLine: 
         lineNumber.append(str(i)[2:-3])
     
-    print(lineNumber)
-    
     if not lineNumber == []:
+        print("You can reach your destination by lines:")
+        print(lineNumber)
         chooseLine = checkInput(lineNumber)
         
         busStop=crsr.execute("SELECT s.StopName FROM StopDepartures s JOIN variants v using(LineName) where s.LineName=? group by s.PointId order by s.No ",(chooseLine,))
@@ -60,6 +87,8 @@ def connectToData():
             busStops.append(str(i)[2:-3])
         #funkcja odpowiedziala za pokazanie iloci przystanków do celu
         busStopsDisplay(busStops,inpFrom,inpTo)
+        #obliczenie ceny biletu
+        ticketCosts()
     else:
         print("We don't have such connection")
         
