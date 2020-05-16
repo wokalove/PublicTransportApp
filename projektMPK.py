c2b5530da6ade6ddd18084c0a6e6d87213452091
wokalove <oladuda1999@gmail.com>
2020-05-16 20:20:12 +0200

    Update projektMPK.py

--- projektMPK.py
+++ projektMPK.py
@@ -17,10 +17,13 @@ def checkInput(lineNumbers):
             print("Wrong input!")
             return checkInput(lineNumbers)
         else:
-            return chooseLine
-            
-def ticketCosts():
-    choice=input("Are you a student? Type 'yes' or 'no'.\n").lower()   
+            return chooseLine          
+        
+def getTicketCosts():
+    finalCosts=np.sum(Traveler.journeyCosts)
+    return finalCosts
+def ticketCosts(choice):
+    #choice=input("Are you a student? Type 'yes' or 'no'.\n").lower()   
     try:
         if(choice == "yes"):
             cost = Traveler.journeyCosts.append(Traveler.student)
@@ -30,10 +33,9 @@ def ticketCosts():
             raise Error
     except Error:
         print("Wrong input, try again!")
-        ticketCosts()
+        ticketCosts(choice)
     if choice in ["yes","no"]:
         finalCosts=np.sum(Traveler.journeyCosts)
-        print("Your all travel costs:",finalCosts)
     
 def busStopsDisplay(busStops,inpFrom,inpTo):
     fromIndex=busStops.index(inpFrom)
@@ -59,7 +61,6 @@ def connectToData():
     inpFrom = input("Give station from:")
     inpTo = input("Give station to:")
     
-    
     #wykonuje zapytanie w sql
     numberLine=crsr.execute("SELECT DISTINCT LineName FROM StopDepartures WHERE StopName=? AND LineName IN (SELECT LineName from StopDepartures  where StopName=?) COLLATE NOCASE",(inpFrom,inpTo,))
     #zwraca wszystkie rekordy znalezione na wskutek zapytania
@@ -71,6 +72,7 @@ def connectToData():
         lineNumber.append(str(i)[2:-3])
     
     if not lineNumber == []:
+        #szukanie bezporedniego połączenia między stacjami
         print("You can reach your destination by lines:")
         print(lineNumber)
         chooseLine = checkInput(lineNumber)
@@ -81,22 +83,39 @@ def connectToData():
         busStops=[]
         print("Bus stops leading to your destination:")
         for i in returnBusStops: 
-            busStops.append(str(i)[2:-3])
+            busStops.append(i)
+            #busStops.append(str(i)[2:-3])
+        bus = [str(i)[2:-3]for i in busStops]
+        print(bus)
         #funkcja odpowiedziala za pokazanie iloci przystanków do celu
-        busStopsDisplay(busStops,inpFrom,inpTo)
+        busStopsDisplay(bus,inpFrom,inpTo)
         #obliczenie ceny biletu
-        ticketCosts()
+        choice=input("Are you a student? Type 'yes' or 'no'.\n").lower()  
+        ticketCosts(choice)
+        print("Final costs of your trip: {} zł".format(getTicketCosts()))
     else:
+        #szukanie poredniego połączenia 
         print("Indirect connection!")
+        
         with open('graf.json',encoding="utf8") as json_file:
             data = json.load(json_file)
+            newdict={}
             newdict = making_graph_from_file_text(data)
-            shortestWay = find_shortest_path(newdict, inpFrom, inpTo)
-            fullPath = path_with_correct_lines(shortestWay,newdict)
-            message(shortestWay,fullPath,inpFrom,inpTo)
+            
+            if all (inp not in newdict for inp in (inpFrom,inpTo)):
+                print("We don't have such connection!!! Try again:")
+                connectToData()
+            else:
+                shortestWay = find_shortest_path(newdict, inpFrom, inpTo)
+                fullPath = path_with_correct_lines(shortestWay,newdict)
+                message(shortestWay,fullPath,inpFrom,inpTo)
 
 
 def message(shortPath,longPath,From,To):
+    journeyCosts=[]
+    choice=input("Are you a student? Type 'yes' or 'no'.\n").lower()   
+    
+    
     print("Path to your destination:" , shortPath)
     print("\n")
     print("You can reach your destination by lines:")
@@ -107,26 +126,30 @@ def message(shortPath,longPath,From,To):
     already = 0
     for i in range(len(shortPath)-1):
         
-        
         if(already == 0):
             lines = np.intersect1d(longPath[shortPath[i]], longPath[shortPath[i+1]])
             cor = longPath[shortPath[i]]
         else:
             lines = np.intersect1d(lines, longPath[shortPath[i+1]])
             
-            
         if (lines.size > 0):
             cor = lines
             already = 1
             continue
-        
+        lines = []
+        #usuwanie duplikatów za pomocą list comprehensions
+        [lines.append(x) for x in cor if x not in lines]
         already = 0
         To = shortPath[i+1]
         
-        print(From," --> ",To," via lines: ",cor)
+        print(From," --> ",To," via lines: ",lines)
+        ticketCosts(choice)
+        
         print()
         
         From = shortPath[i+1]
+    print("Final costs of your trip: {} zł".format(getTicketCosts()))
+        
             
             
 def find_shortest_path(graph, start, end):
