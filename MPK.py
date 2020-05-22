d242a12aeb1a23bab557009fbab07a93dcfef3f3
wokalove <oladuda1999@gmail.com>
2020-05-22 09:49:40 +0200

    Update MPK.py

--- MPK.py
+++ MPK.py
@@ -4,9 +4,6 @@ import sqlite3
 import numpy as np
 import json
 
-class Error(Exception):
-    """Base class for exceptions"""
-
 class Traveler:
     '''Storing price of tickets and final journey costs. Moreover, functions 
     resposible for counting costs of journey. These aren't time tickets,
@@ -16,7 +13,7 @@ class Traveler:
     REDUCED_PRICE = 2.30
     __journey_costs = []
 
-    def ticket_Costs(choice):
+    def ticket_costs(choice):
         '''Function which count costs of journey depend on user's input.'''
         final_costs=[]
         if choice == "yes":
@@ -29,7 +26,7 @@ class Traveler:
             final_costs = sum(Traveler.__journey_costs)
         return final_costs
 
-    def get_Ticket_Costs():
+    def get_ticket_costs():
         ''' Returninng final costs of journey'''
         final_costs = sum(Traveler.__journey_costs)
         return final_costs
@@ -46,12 +43,16 @@ class Window1:
         self.master.title("My app")
         self.master.config(bg="black")
         self.master.resizable(False, False)
+        self.background_color = "black"
+        self.font_type = "Courier"
         
-        self.frame = tk.Frame(width=1000, height=1000, background="black")
+        self.frame = tk.Frame(width=1000, height=1000, 
+                              background=self.background_color)
         
-        self.label= tk.Label(self.master,text="Travelling in Cracow",bg = "black",fg = "white")
+        self.label= tk.Label(self.master,text="Travelling in Cracow",
+                             bg=self.background_color,fg="white")
         self.label.pack(pady=20)
-        self.label.config(font=("Courier",25))
+        self.label.config(font=( self.font_type ,25))
         
         self.image= Image.open('pociąg.jpg')
         self.render= ImageTk.PhotoImage(self.image)
@@ -59,8 +60,11 @@ class Window1:
         self.img_label.pack(pady=20)
         
         self.create_new_button("Start", "2", Window2)
-        self.quit = tk.Button(self.frame, text = f"Quit", command = self.close_window,height = 2, width = 10,fg="red")
-        self.quit.config(font=("Courier",20))
+        self.quit = tk.Button(self.frame, 
+                              text=f"Quit",
+                              command=self.close_window,
+                              height=2, width=10,fg="red")
+        self.quit.config(font=(self.font_type, 20))
         
         self.quit.pack(side=tk.TOP,pady=20)
         self.frame.pack( padx=20, pady=20)
@@ -68,7 +72,8 @@ class Window1:
         
     def create_new_button(self, text, number, _class):
         ''' Creating new button with lambda expression'''
-        b=tk.Button(self.frame, text = text,height = 2, width = 10,fg="red",command= lambda:self.new_window(number, _class))
+        b=tk.Button(self.frame, text = text,height = 2, width = 10,fg="red",
+                    command= lambda:self.new_window(number, _class))
         b.config(font=("Courier",20))
         b.pack(side=tk.TOP)
         
@@ -90,39 +95,55 @@ class Window2(Window1):
         self.master.config(bg="black")
         self.master.geometry("600x600+300+10")
         self.master.resizable(False,False)
+        self.background_color = "black"
+        self.font_color = "white"
+        self.font_type = "Courier"
         
+        self.frame = tk.Frame(self.master,width=500, height=500, 
+                              background=self.background_color)
         
-        self.frame = tk.Frame(self.master,width=500, height=500, background="black")
-        
-        self.student_label= tk.Label(self.master,text="Are you a student?\nType 'yes' or 'no'.",bg = "black",fg = "white")
+        self.student_label= tk.Label(self.master,
+                                     text=
+                                     "Are you a student?\nType 'yes' or 'no'.",
+                                     bg = self.background_color,fg="white")
         self.student_label.place(x=160,y=50)
-        self.student_label.config(font=("Courier",20))
+        self.student_label.config(font=(self.font_type,20))
     
         
         self.student_place = tk.Entry(self.master,bd=20)
         self.student_place.place(x=220,y=130)
         
-        self.connection_label= tk.Label(self.master,text="Connection",bg = "black",fg = "white")
+        self.connection_label= tk.Label(self.master,text="Connection",
+                                        bg = self.background_color,
+                                        fg = self.font_color)
         self.connection_label.place(x=180,y=200)
-        self.connection_label.config(font=("Courier",30))        
+        self.connection_label.config(font=(self.font_type,30))        
         
-        self.from_label= tk.Label(self.master,text="From:",bg = "black",fg = "white")
+        self.from_label= tk.Label(self.master,text="From:",
+                                  bg= self.background_color,
+                                  fg = self.font_color)
         self.from_label.place(x=100,y=280)
-        self.from_label.config(font=("Courier",30))
+        self.from_label.config(font=(self.font_type,30))
         
         self.from_place = tk.Entry(self.master,bd=20)
         self.from_place.place(x=240,y=280)
         
-        self.to_label= tk.Label(self.master,text="To:",bg = "black",fg = "white")
+        self.to_label= tk.Label(self.master,text="To:",
+                                bg = self.background_color,
+                                fg = self.font_color)
         self.to_label.place(x=140,y=380)
-        self.to_label.config(font=("Courier",30))
+        self.to_label.config(font=(self.font_type,30))
         
         self.to_place = tk.Entry(self.master,bd=20)
         self.to_place.place(x=240,y=380)
         
 
         self.create_new_button("Find connection -->", "2", Win3)
-        return_button = tk.Button(self.master,font=("Courier",12), text = "<--- Back",height = 2, width = 12,fg="red",command= lambda:self.return_window(self.master))
+        return_button = tk.Button(self.master,font=(self.font_type,12), 
+                                  text="<--- Back",height=2,
+                                  width=12,fg="red",
+                                  command=
+                                  lambda:self.return_window(self.master))
         return_button.place(x=150,y=480)
         
         self.frame.pack( padx=50, pady=200)
@@ -133,24 +154,35 @@ class Window2(Window1):
         _class(self.new, number,answers) 
         
     def return_window(self,master): 
-        ''' Function which return to last window - this function is called after clinking on specific button'''
+        ''' Function which return to last window - this function is called 
+        after clinking on specific button'''
         master.withdraw()
 
         
     def create_new_button(self, text, number, _class):
         ''' overriden button creating function from base class  '''
-        b = tk.Button(self.master, text = text,height = 2, width = 20,fg="red",command= lambda:self.new_window(number, _class,self.get_user_answers()))
-        b.config(font=("Courier",12))
+        b = tk.Button(self.master, text=text,height=2,width=20,
+                      fg="red",
+                      command= lambda:self.new_window(number, _class,
+                                                      self.get_user_answers()))
+        b.config(font=( self.font_type ,12))
         b.place(x=300,y=480)
         
     def show_lines(self,inp_from,inp_to,if_student):
-        ''' Function which is called on button and displays all stops from connect_to_data function'''
-        l = tk.Label(self.master, text ="I am looking for connection..",font=("Courier",12),bg="white", fg="black",wraplength=600)
+        ''' Function which is called on button and displays 
+            all stops from connect_to_data function'''
+        l = tk.Label(self.master, 
+                     text ="I am looking for connection..",
+                     font=("Courier",12),
+                     bg="white", fg="black",wraplength=600)
         text=str(self.connect_to_data(inp_from,inp_to,if_student)).replace("]","").replace("[","")
-        l.config( text = text)
+        l.config(text=text)
 
         if text == []:
-            l.config(text="We don't have such connection!:(",wraplength=300,font=("Courier",12))
+            l.config(text="We don't have such connection!:("
+                                                           ,wraplength=300,
+                                                           font=
+                                                           (self.font_type,12))
         l.place(x=300,y=130, anchor="center")
 
     def get_user_answers(self):
@@ -161,7 +193,8 @@ class Window2(Window1):
         answers.append(self.to_place.get())
         
         self.label = tk.Label(self.master)
-        self.label.config(font=("Courier",25),text="Found connection",bg="black", fg="white")
+        self.label.config(font=( self.font_type,25),
+                          text="Found connection",bg="black", fg="white")
         self.label.pack(pady=20)
         print(answers)
         return answers
@@ -195,12 +228,19 @@ class Window2(Window1):
                 
                 
                 if all (inp not in new_dict for inp in (inp_from,inp_to)):
-                    l = tk.Label(self.master, text ="We don't have such connection...",font=("Courier",12),bg="white", fg="black",wraplength=600)
+                    l = tk.Label(self.master,
+                                 text ="We don't have such connection...",
+                                 font=( self.font_type ,12),
+                                 bg="white", fg="black",
+                                 wraplength=600)
                     l.place(x=300,y=130, anchor="center")
                 else:
-                    shortest_way = self.find_shortest_path(new_dict, inp_from, inp_to)
-                    fullPath = self.path_with_correct_lines(shortest_way,new_dict)
-                    self.display_bus_stops_indirect(shortest_way,fullPath,inp_from,inp_to,if_student)
+                    shortest_way = self.find_shortest_path(new_dict, 
+                                                           inp_from, inp_to)
+                    fullPath = self.path_with_correct_lines(shortest_way,
+                                                            new_dict)
+                    self.display_bus_stops_indirect(shortest_way,fullPath,
+                                                    inp_from,inp_to,if_student)
                     
     
     def find_stops(self,number,inp_from,inp_to,if_student):
@@ -221,16 +261,19 @@ class Window2(Window1):
         bus = [str(i)[2:-3]for i in bus_stops]
 
         self.bus_stops_display(bus,inp_from,inp_to)
-        Traveler.ticket_Costs(if_student)
+        Traveler.ticket_costs(if_student)
         
-        text= 'Cost of journey:'+str(Traveler.get_Ticket_Costs())
-        cost_lab = tk.Label(self.master, text = text,font=("Courier",12),bg="black", fg="white",wraplength=300)
+        text= 'Cost of journey:'+str(Traveler.get_ticket_costs())
+        cost_lab = tk.Label(self.master,text=text,
+                            font=( self.font_type ,12),
+                            bg="black", fg="white",wraplength=300)
         cost_lab.place(x=300,y=540, anchor="center")
         
 
     def bus_stops_display(self,busStops,inpFrom,inpTo):
         ''' displaying bust stops in direct connection  there and back'''
-        l = tk.Label(self.master, text = "Write down line of tram/bus!!!",font=("Courier",8),bg="white", fg="black")
+        l = tk.Label(self.master, text = "Write down line of tram/bus!!!",
+                     font=( self.font_type,8),bg="white", fg="black")
         l.place(x=300,y=390, anchor="center")
         
         from_index=busStops.index(inpFrom)
@@ -253,8 +296,9 @@ class Window2(Window1):
             l.config(text=text,wraplength=600)
 
     def making_graph_from_file_text(self,text):
-        ''' Making new_dict from dictionary {nr_line: next_stops} from .json file
-            new_dict are conections specific line between neighboring pairs of stops'''
+        ''' Making new_dict from dictionary {nr_line: next_stops}
+            from .json file new_dict are conections specific line 
+            between neighboring pairs of stops'''
         new_dict = {}
 
         for line_number,stops in text.items():
@@ -306,7 +350,8 @@ class Window2(Window1):
         
         return full_path
     def display_bus_stops_indirect(self,short_path,long_path,From,To,if_student):
-        ''' Displaying path of indirect connections  ith removing duplicate lines'''
+        ''' Displaying path of indirect connections
+            with removing duplicate lines'''
         print("Path to your destination:" , short_path)
         print("\n")
         print("You can reach your destination by lines:")
@@ -319,7 +364,8 @@ class Window2(Window1):
         for i in range(len(short_path)-1):
             
             if(already == 0):
-                lines = np.intersect1d(long_path[short_path[i]], long_path[short_path[i+1]])
+                lines = np.intersect1d(long_path[short_path[i]],
+                                       long_path[short_path[i+1]])
                 cor = long_path[short_path[i]]
             else:
                 lines = np.intersect1d(lines, long_path[short_path[i+1]])
@@ -338,13 +384,17 @@ class Window2(Window1):
            
             text=str(From) + "-->" +str(To) +"VIA LINES" + str(lines)
             stops.append(text)
-            Traveler.ticket_Costs(if_student)
+            Traveler.ticket_costs(if_student)
             From = short_path[i+1]
-        final_costs_text = "Final costs are:" + str(Traveler.get_Ticket_Costs())
+        final_costs_text = "Final costs are:" + str(Traveler.get_ticket_costs())
         stops.append(final_costs_text)
         text = str(stops).replace("{","").replace("}", "").replace('[',"").replace(']',"").replace('"',"")
 
-        stopsLabel = tk.Label(self.master, text = text,font=("Courier",20),bg="black", fg="white",width=200,height=200,wraplength =450)
+        stopsLabel = tk.Label(self.master, 
+                              text=text,
+                              font=(self.font_type,20),
+                              bg="black", fg="white",
+                              width=200,height=200,wraplength =450)
         stopsLabel.place(x=300,y=300, anchor="center")
 
     def save_to_file_ONLY_ONCE():
@@ -364,7 +414,8 @@ class Window2(Window1):
             bus_stop=crsr.execute("SELECT s.StopName FROM StopDepartures s JOIN variants v using(LineName) where s.LineName=? group by s.PointId order by s.No ",(line,))
             return_bus_stops= crsr.fetchall()  
             graph[line]=return_bus_stops
-            json.dump( graph, open( 'graf.json', 'w' ,encoding='utf8'),ensure_ascii=False )
+            json.dump( graph, open( 'graf.json', 'w',encoding='utf8'),
+                      ensure_ascii=False )
         
 class Win3(Window2):
     def __init__(self, master, number,studentAns):
@@ -374,36 +425,66 @@ class Win3(Window2):
         self.master.config(bg="black")
         self.master.resizable(False,False)
 
-        self.label_found = tk.Label(self.master)
-        self.label_found.config(font=("Courier",25),text="Found connections",bg="black", fg="white")
-        self.label_found.pack(pady=10)
+        self.background_color = "black"
+        self.font_color = "white"
+        self.font_type = "Courier"
 
-        b = tk.Button(self.master, text="Show Lines", command=lambda:self.show_lines(self.studentAns[1],self.studentAns[2],self.studentAns[0]),height=1,width=10,fg="red",bg="white")
-        b.config(font=("Courier",12))
+        self.label_found = tk.Label(self.master)
+        self.label_found.config(font=(self.font_type,25),
+                                text="Found connections",
+                                bg=self.background_color, 
+                                fg=self.font_color)
+        self.label_found.pack(pady=10)
+        self.studentAns = studentAns
+
+        b = tk.Button(self.master, 
+                      text="Show Lines",
+                      command=lambda:self.show_lines(self.studentAns[1],
+                                                     self.studentAns[2],
+                                                     self.studentAns[0]),
+                      height=1,
+                      width=10,
+                      fg="red",
+                      bg="white")
+        b.config(font=(self.font_type,12))
         b.pack(pady=2)
 
         self.label_type_line = tk.Label(self.master)
-        self.label_type_line.config(font=("Courier",25),text="Type line from given above:",bg="black", fg="white")
+        self.label_type_line.config(font=(self.font_type,25),
+                                    text="Type line from given above:",
+                                    bg=self.background_color,
+                                    fg=self.font_color)
         self.label_type_line.place(x=40,y=150)
                 
         self.choose_line_place = tk.Entry(self.master,bd=20)
         self.choose_line_place.place(x=220,y=200)
 
-        show_button = tk.Button(self.master, text = f"Show Stops",height = 1, width = 10,fg="red", command = lambda : self.find_stops(self.choose_line_place,self.studentAns[1],self.studentAns[2],self.studentAns[0]))
+        show_button = tk.Button(self.master,
+                                text = f"Show Stops",
+                                height = 1, width = 10,
+                                fg="red", 
+                                command=
+                                lambda: self.find_stops(self.choose_line_place,
+                                                                self.studentAns[1],
+                                                                self.studentAns[2],
+                                                                self.studentAns[0]))
         show_button.place(x=400,y=210)
-        show_button.config(font=("Courier",15))
+        show_button.config(font=(self.font_type,15))
         
 
-        self.quit_button = tk.Button(self.master, text = f"Quit",height = 1, width = 10,fg="red", command = self.close_window)
-        self.quit_button.configure(font=("Courier",12))
+        self.quit_button = tk.Button(self.master,
+                                     text = f"Quit",
+                                     height = 1, width = 10,fg="red",
+                                     command = self.close_window)
+        self.quit_button.configure(font=(self.font_type,12))
         self.quit_button.place(x=320,y=560)
         
-        back_button = tk.Button(self.master,font=("Courier",12), text = "<--- Back",height = 1, width = 10,fg="red",command= lambda:self.return_window(self.master))
+        back_button = tk.Button(self.master,
+                                font=(self.font_type,12), 
+                                text = "<--- Back",height = 1, 
+                                width = 10,fg="red",
+                                command= lambda:self.return_window(self.master))
         back_button.place(x=180,y=560)
-        
-    def close_window(self):
-        root.destroy()
-        
 
 def main():
     root = tk.Tk()
